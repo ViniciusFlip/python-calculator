90586b3436d59bae8a9ee235feb6668c0a9e4572
VHarmony <harmony.dgt@gmail.com>
2026-02-15 18:21:06 -0300

    ..

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-#Calculator Python
+#alculator in Python
+
+Simple calculator project developed in Python, focusing on learning the language and using Git/GitHub.
+
+## Features
+- Addition
+- Subtraction
+- Multiplication
+- Division
+- Continuous loop until the user exits
+
+## How to run
+
+```bash
+python calculator.py
--- calculator.py
+++ calculator.py
@@ -1,1 +1,59 @@
-print('Hellow World')
+#obctive to create a smart calculator using python
+
+def show_menu(): 
+    print('/n===Calculadora Smart Python ===')
+    print("1 - Soma")
+    print("2 - Subtração")
+    print("3 - Multiplicação")
+    print("4 - Divisão")
+    print("0 - Sair")
+
+
+def sum(a,b):
+    return a + b
+
+def subtraction(a,b):
+    return a - b
+ 
+def multiplication(a,b):
+    return a * b
+
+def division(a, b):
+    if b == 0:
+        return "Erro: divisão por zero!"
+    return a / b
+
+
+
+while True:
+
+    show_menu()
+
+    opcao=input("Escolha uma opção")
+
+    if opcao == 0:
+        print('Encerrando calculadora')
+        break
+
+    if opcao not in ["1","2","3","4"]:
+        print("opção inválida") 
+        continue
+
+    num1=float(input("Digite o primeiro número:"))
+    num2=float(input("Digite o segundo número:"))
+
+
+    if opcao == "1":
+       resultado = sum(num1,num2)
+    
+
+    if opcao == "2":
+       resultado = subtraction(num1, num2)
+
+    if opcao == "3":
+       resultado = multiplication(num1, num2)
+
+    if opcao == "4":
+       resultado = division(num1, num2)
+
+    print("resultado", resultado)
